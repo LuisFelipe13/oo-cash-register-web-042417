da75141d304b4625794d787f8839f7e75c987e42
Luis del Cueto <luisdelcueto@live.com>
2017-04-28 11:06:45 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+  attr_accessor :total, :discount, :items
 
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount if discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    quantity.times {@items << title}
+    @total += (price * quantity)
+    @price = price
+  end
+
+  def apply_discount
+    return "There is no discount to apply." if discount == nil
+    discount = @discount.to_f/100
+    @total = (@total - (@total * discount)).to_i
+    "After the discount, the total comes to $#{@total}."
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total -= @price
+  end
+
+end
